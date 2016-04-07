fa343b319148e01e3864f457fc3ac2f25eabbf59
Valerie <val.conklin@gmail.com>
2016-04-07 16:36:46 -0700

    Fixed score_many method to make it more DRY and got it WORKING YEAH WOOOOOO

--- lib/score.rb
+++ lib/score.rb
@@ -9,11 +9,13 @@ class Score
   5 => ["K"],
   8 => ["J", "X"],
   10 => ["Q", "Z"]
-}
+  }
 
   def initialize(word)
     @word = word
   end
+
+  ## Both score methods can probably be refactored to work with a string instead of always converting the individual words to arrays. DO THIS SOON.
 
   def self.score(word)
   score = 0
@@ -32,24 +34,20 @@ class Score
   # if all seven tiles are used the below tenary will add 50 points to the users score else return score
   tiles == 7 ? score += 50 : score
   score
-end
-
-def self.score_many(multiple_word_string)
-## for many words, the user writes their words in a textarea, and give them instructions to separate the words with a space
-  multiple_words = multiple_word_string.upcase.split(" ")
-  split_into_letters = multiple_words.map do |word|
-    word.split("")
   end
 
-  ## This can all probably be refactored to work with a string instead of always converting the individual words to arrays. DO THIS SOON.
-  split_into_letters.each do |letter|
-    LETTER_POINTS.each do |points, letters_array|
-      if letters_array.include? letter
-        score += points
-        tiles += 1
-      end
+  def self.score_many(multiple_word_string)
+  ## for many words, the user writes their words in a textarea, and give them instructions to separate the words with a space
+    multiple_words = multiple_word_string.upcase.split(" ")
+    split_into_letters = multiple_words.map do |word|
+      word.split("")
     end
+
+    multiple_words = multiple_word_string.split(" ")
+    word_scores = multiple_words.map do |word|
+      self.score(word)
+    end
+    word_scores.reduce(0,:+)
   end
-end
 
 end
